82314b74076b937fddf476a3dc38a684d7bb205b
Frank LaNasa <frank@Franks-MacBook-Pro.local>
2018-04-04 18:30:35 -0400

    object.assign polyfill

--- index.ts
+++ index.ts
@@ -2,7 +2,35 @@ import { extend, isFunction, isNaN, isObject, isPlainObject, isString } from 'lo
 import { Currency } from './lib/currency'
 import { Currencies } from './lib/currencies'
 
+if (typeof Object.assign != 'function') {
+  // Must be writable: true, enumerable: false, configurable: true
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) { // .length of function is 2
+      'use strict';
+      if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
 
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+          for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
 
 let isInt = function (n) {
     return Number(n) === n && n % 1 === 0
